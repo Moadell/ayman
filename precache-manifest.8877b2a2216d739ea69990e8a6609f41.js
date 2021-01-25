5efe15ef51a03de05330c06d9c64d6b325eab581
Moadell <mo.adel@nu.edu.eg>
2021-01-25 18:41:36 +0200

    Updates

---
+++ precache-manifest.8877b2a2216d739ea69990e8a6609f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a746e20d14db1fc7b7aff460fc0d6d7",
+    "url": "/ayman/index.html"
+  },
+  {
+    "revision": "e213cc46e94148c4efe3",
+    "url": "/ayman/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4c7f4d6b3df89f44b70b",
+    "url": "/ayman/static/js/2.d81c19b1.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/ayman/static/js/2.d81c19b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e213cc46e94148c4efe3",
+    "url": "/ayman/static/js/main.72cb59a7.chunk.js"
+  },
+  {
+    "revision": "9145106b8eb72a87914f",
+    "url": "/ayman/static/js/runtime-main.80987d42.js"
+  }
+]);
